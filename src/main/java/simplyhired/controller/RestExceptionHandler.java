490bec5fb3d18a259cfb95a38282e12d10c3b791
mans4321 <t6t6t.sa@gmail.com>
2019-07-25 23:11:19 -0400

    add RestExceptionHandler

---
+++ src/main/java/simplyhired/controller/RestExceptionHandler.java
@@ -0,0 +1,21 @@
+package simplyhired.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import simplyhired.model.Exception.ErrorResponse;
+
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handlerException(Exception ex){
+        ErrorResponse err = new ErrorResponse();
+        err.setStatus(HttpStatus.BAD_REQUEST.value());
+        err.setMessage(ex.getMessage());
+        err.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<> (err, HttpStatus.BAD_REQUEST);
+    }
+}
